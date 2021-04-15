e518e3b278c775e418a9773f486cf7c2c509fb4d
Dane Brophy <heartbeat@Danes-MacBook-Pro.local>
2021-04-15 09:14:51 -0600

    Create name method

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,5 +1,7 @@
 class Ride
+  attr_reader :name
 
   def initialize(ride_info)
+    @name = ride_info[:name]
   end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe Ride do
       expect(ride).to be_an_instance_of(Ride)
     end
   end
+
+  describe '#methods' do
+    it '#has a name' do
+      ride = Ride.new({name: 'Ferris Wheel', cost: 0})
+
+      expect(ride.name).to eq("Ferris Wheel")
+    end
+  end
 end
