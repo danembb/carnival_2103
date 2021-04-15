4fa9807b4b76f0e5ec03b19b07d04ccd80e2cf51
Dane Brophy <heartbeat@Danes-MacBook-Pro.local>
2021-04-15 09:36:52 -0600

    Create name method for carnival

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe do
       expect(jeffco_fair).to be_an_instance_of(Carnival)
     end
   end
+
+  describe '#methods' do
+    it '#name' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+
+      expect(jeffco_fair.name).to eq("Jefferson County Fair")
+    end
+  end
 end
