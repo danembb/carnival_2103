ddca4fec565437d98cf4c61e3ae803c216a8b060
Dane Brophy <heartbeat@Danes-MacBook-Pro.local>
2021-04-15 09:49:17 -0600

    Create add_ride method to update jeffco_fair carnival rides

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,9 +1,13 @@
 class Carnival
   attr_reader :name,
               :rides
-              
+
   def initialize(name)
     @name = name
     @rides = []
+  end
+
+  def add_ride(new_ride)
+    @rides.push(new_ride)
   end
 end
